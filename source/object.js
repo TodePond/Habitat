55f3065fdede48806d865b804697cd39c1a0009c
Luke Wilson <l2wilson94@gmail.com>
2021-02-17 11:47:44 +0000

    object

--- source/object.js
+++ source/object.js
@@ -5,11 +5,40 @@
 	Habitat.Object = {}
 	Habitat.Object.install = (global) => {
 		
-		Object.prototype[Symbol.iterator] = function*() {
-			for (const key in this) {
-				yield this[key]
+		Reflect.defineProperty(global.Object.prototype, Symbol.iterator, {
+			value: function*() {
+				for (const key in this) {
+					yield this[key]
+				}
 			}
-		}
+		}, {configurable: true, enumerable: false, writable: true})
+		
+		Reflect.defineProperty(global.Object.prototype, "keys", {
+			get() {
+				return Object.keys(this)
+			},
+			set(value) {
+				Reflect.defineProperty(this, "keys", {value, configurable: true, writable: true, enumerable: true})
+			}
+		}, {configurable: true, enumerable: false, writable: true})
+		
+		Reflect.defineProperty(global.Object.prototype, "values", {
+			get() {
+				return Object.values(this)
+			},
+			set(value) {
+				Reflect.defineProperty(this, "values", {value, configurable: true, writable: true, enumerable: true})
+			}
+		}, {configurable: true, enumerable: false, writable: true})
+		
+		Reflect.defineProperty(global.Object.prototype, "entries", {
+			get() {
+				return Object.entries(this)
+			},
+			set(value) {
+				Reflect.defineProperty(this, "entries", {value, configurable: true, writable: true, enumerable: true})
+			}
+		}, {configurable: true, enumerable: false, writable: true})
 		
 		Habitat.Object.installed = true
 		
