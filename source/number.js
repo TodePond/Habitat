dfed1df61950777b39116749db9ca1c1e7d74ebf
Luke Wilson <l2wilson94@gmail.com>
2021-03-27 19:17:08 +0000

    remove mothertode and term

--- source/number.js
+++ source/number.js
@@ -26,6 +26,32 @@
 			writable: true,
 		})
 		
+		const numberToString = global.Number.prototype.toString
+		Reflect.defineProperty(global.Number.prototype, "toString", {
+			value(base, size) {
+				if (size === undefined) return numberToString.call(this, base)
+				if (size <= 0) return ""
+				const string = numberToString.call(this, base)
+				return string.slice(-size).padStart(size, "0")
+			},
+			configurable: true,
+			enumerable: false,
+			writable: true,
+		})
+		
+		const bigIntToString = global.BigInt.prototype.toString
+		Reflect.defineProperty(global.BigInt.prototype, "toString", {
+			value(base, size) {
+				if (size === undefined) return bigIntToString.call(this, base)
+				if (size <= 0) return ""
+				const string = bigIntToString.call(this, base)
+				return string.slice(-size).padStart(size, "0")
+			},
+			configurable: true,
+			enumerable: false,
+			writable: true,
+		})
+		
 		Habitat.Number.installed = true
 		
 	}
