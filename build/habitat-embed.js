c27ffaa5b12efc79accdaa075b3acdfce6d747a4
Luke Wilson <l2wilson94@gmail.com>
2021-11-15 19:49:22 +0000

    make

--- build/habitat-embed.js
+++ build/habitat-embed.js
@@ -945,25 +945,72 @@ Habitat.install = (global) => {
 {
 	Habitat.Tween = {}
 	
+	let tt = true
+	Habitat.Tween.getSteps = ({from, to, over, launch, land} = {}) => {
+		if (to === undefined) to = this[propertyName]
+		if (from === undefined) from = this[propertyName]
+		if (over === undefined) over = 1000
+		if (launch === undefined) launch = 1.0
+		if (land === undefined) land = 1.0
+
+		const difference = to - from
+		const length = 60 * over/1000
+		
+		const jump = difference / length
+		const jumps = [jump].repeat(length)
+
+		for (let i = 0; i < length; i++) {
+			const j = jumps[i]
+
+			const startness = (length-(i*land)) / length
+			const endness = (i*launch+1) / length
+
+			jumps[i] = j * (endness * startness)
+		}
+
+		const steps = []
+		let v = 0
+		for (const j of jumps) {
+			v += j
+			steps.push(v)
+		}
+
+		const error = to / steps[steps.length-1]
+		for (let i = 0; i < steps.length; i++) {
+			steps[i] *= error
+		}
+
+		/*for (const s of jumps) {
+			if (tt) print("=".repeat(s*100))
+			else print("-".repeat(s*100))
+			tt = !tt
+		}*/
+
+		/*for (const s of steps) {
+			if (tt) print("=".repeat(s))
+			else print("-".repeat(s))
+			tt = !tt
+		}*/
+
+		return steps
+	}
+
 	Habitat.Tween.install = (global) => {
 		Habitat.Tween.installed = true
 
 		Reflect.defineProperty(global.Object.prototype, "tween", {
-			value(propertyName, {from, to, over} = {}) {
-				if (to === undefined) to = this[propertyName]
-				if (from === undefined) from = this[propertyName]
-				if (over === undefined) over = 1000
-				over = 60 * over/1000
-				const difference = to - from
+			value(propertyName, options) {
 
+				const steps = Habitat.Tween.getSteps(options)
+				
 				const promise = new Promise((resolve) => {
 					let i = 0
 					const tweener = setInterval(() => {
 						
-						this[propertyName] = from + i*difference/over
+						this[propertyName] = steps[i]
 						
 						i++
-						if (i > over) {
+						if (i >= steps.length) {
 							clearInterval(tweener)
 							resolve()
 						}
