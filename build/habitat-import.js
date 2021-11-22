ce7f84f7f1c70347217be831f209c178ca7d4d1f
Luke Wilson <l2wilson94@gmail.com>
2021-11-22 18:02:43 +0000

    allow for 0 values of to and from

--- build/habitat-import.js
+++ build/habitat-import.js
@@ -949,9 +949,9 @@ Habitat.install = (global) => {
 		Habitat.Tween.installed = true
 
 		Reflect.defineProperty(global.Object.prototype, "tween", {
-			value(propertyName, {to, from, over = 1000, launch = 0.5, land = 0.5}) {
-				if (!from) from = this[propertyName]
-				if (!to) to = this[propertyName]
+			value(propertyName, {to, from, over = 1000, launch = 0.5, land = 0.5} = {}) {
+				if (from === undefined) from = this[propertyName]
+				if (to === undefined) to = this[propertyName]
 
 				launch *= 2/3
 				land = 1/3 + (1 - land) * 2/3
--- source/tween.js
+++ source/tween.js
@@ -9,8 +9,8 @@
 
 		Reflect.defineProperty(global.Object.prototype, "tween", {
 			value(propertyName, {to, from, over = 1000, launch = 0.5, land = 0.5} = {}) {
-				if (!from) from = this[propertyName]
-				if (!to) to = this[propertyName]
+				if (from === undefined) from = this[propertyName]
+				if (to === undefined) to = this[propertyName]
 
 				launch *= 2/3
 				land = 1/3 + (1 - land) * 2/3
