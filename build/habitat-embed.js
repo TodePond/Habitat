da80ec070f130822a66003b24ac66952f93ac81f
Luke Wilson <l2wilson94@gmail.com>
2021-11-22 18:08:07 +0000

    changed dynamically written function to normal function

--- build/habitat-embed.js
+++ build/habitat-embed.js
@@ -950,33 +950,30 @@ Habitat.install = (global) => {
 
 		Reflect.defineProperty(global.Object.prototype, "tween", {
 			value(propertyName, {to, from, over = 1000, launch = 0.5, land = 0.5} = {}) {
-				if (from === undefined) from = this[propertyName]
-				if (to === undefined) to = this[propertyName]
+				const before = this[propertyName]
+				if (from === undefined) from = before
+				if (to === undefined) to = before
 
 				launch *= 2/3
 				land = 1/3 + (1 - land) * 2/3
 
+				const start = performance.now()
+
 				Reflect.defineProperty(this, propertyName, {
-					get: new Function(`
-						const before = ${this[propertyName]}
-						const start = ${performance.now()}
-						const { from, to, over, launch, land } = ${JSON.stringify({to, from, over, launch, land})}
+					get() {
 						const now = performance.now()
 
 						if (now > start + over) {
-							delete this.${propertyName}
-							this.${propertyName} = to
-							return to;
+							delete this[propertyName]
+							this[propertyName] = to
+							return to
 						} else {
-							let t = (now - start) / over
-
-							let v = 3*t*(1-t)*(1-t)*launch +
-								3*t*t*(1-t)*land + t*t*t
-
+							const t = (now - start) / over
+							const v = 3*t*(1-t)*(1-t)*launch + 3*t*t*(1-t)*land + t*t*t
 							return v * (to - from) + from
 						}
 
-					`),
+					},
 					set(value) {
 						delete this[propertyName]
 						this[propertyName] = to
