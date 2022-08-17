5a9e9cf5ce0d3e7532f776294d26d38d36835b1f
Luke Wilson <l2wilson94@gmail.com>
2022-08-17 17:05:44 +0100

    fix: colour.js doesn't crash when there is no `document` defined anymore

--- tinker/tinker.js
+++ tinker/tinker.js
@@ -1,14 +1,4 @@
 
-Habitat.install(this)
+import {Habitat} from "../build/habitat-import.js"
 
-const stage = Stage.start({scale: 0.8, aspectRatio: [2, 1]})
-stage.tick = (context) => {
-
-    // Clear the screen every frame!
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height)
-    
-    // Draw whatever you want!
-    context.fillStyle = Colour.Blue
-    context.fillRect(0, 0, 10, 10)
-    
-}
+Habitat.install(window)
