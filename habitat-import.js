2e63af4123da645ad25c67fefc25a676e898d5c4
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-02-19 14:35:18 +0000

    refactor stage and use layers

--- habitat-import.js
+++ habitat-import.js
@@ -851,78 +851,112 @@ const HabitatFrogasaurus = {}
 	{
 		HabitatFrogasaurus["./stage.js"] = {}
 		
-		const Stage = function (options) {
-			const properties = {
-				context: undefined,
+		const Stage = class {
+			constructor(options) {
+				const properties = {
+					layers: ["2d"],
 		
-				speed: 1.0,
-				paused: false,
+					speed: 1.0,
+					paused: false,
 		
-				start: () => {},
-				resize: () => {},
-				tick: () => {},
-				update: () => {},
-				...options,
-			}
+					start: () => {},
+					resize: () => {},
+					tick: () => {},
+					update: () => {},
+					...options,
+				}
 		
-			const internal = {
-				clock: 0.0,
-			}
+				const internal = {
+					layers: properties.layers.map((v) => new Layer(v)),
+					clock: 0.0,
+				}
 		
-			const stage = {
-				...internal,
-				...properties,
-			}
-		
-			if (document.body === null) {
-				addEventListener("load", () => {
-					requestAnimationFrame(() => start(stage))
+				Object.assign(this, {
+					...properties,
+					...internal,
 				})
-			} else {
-				requestAnimationFrame(() => start(stage))
+		
+				if (document.body === null) {
+					addEventListener("load", () => {
+						requestAnimationFrame(() => this.fireStart())
+					})
+				} else {
+					requestAnimationFrame(() => this.fireStart())
+				}
 			}
 		
-			return stage
+			getContexts = () => this.layers.map((v) => v.context)
+		
+			fireStart = () => {
+				for (const layer of this.layers) {
+					layer.start(layer.context)
+				}
+		
+				on("resize", () => this.fireResize())
+				on(keyDown(" "), () => (this.paused = !this.paused))
+		
+				this.start(this.getContexts())
+				this.fireResize()
+				this.fireTick()
+			}
+		
+			fireResize = () => {
+				for (const layer of this.layers) {
+					layer.resize(layer.context)
+				}
+		
+				this.resize(this.getContexts())
+			}
+		
+			fireTick = (time) => {
+				this.clock += this.speed
+				while (this.clock > 0) {
+					if (!this.paused) this.update(this.getContexts(), time)
+					this.tick(this.getContexts(), time)
+					this.clock--
+				}
+		
+				requestAnimationFrame((time) => this.fireTick(time))
+			}
 		}
 		
-		const start = (stage) => {
-			// Create a context + canvas if no context was provided
-			if (stage.context === undefined) {
+		const CanvasLayer = class {
+			constructor() {
+				this.type = "2d"
+				this.context = null
+			}
+		
+			start() {
 				const canvas = document.createElement("canvas")
 				canvas.style["background-color"] = "#171d28"
 				document.body.style["background-color"] = "#06070a"
 				document.body.style["margin"] = "0px"
 				document.body.style["overflow"] = "hidden"
 				document.body.appendChild(canvas)
-				stage.context = canvas.getContext("2d")
+				this.context = canvas.getContext("2d")
 			}
 		
-			on("resize", () => resize(stage))
-			on(keyDown(" "), () => (stage.paused = !stage.paused))
-		
-			stage.start(stage.context)
-			resize(stage)
-			tick(stage)
+			resize(context) {
+				const { canvas } = context
+				canvas.width = Math.round(innerWidth)
+				canvas.height = Math.round(innerHeight)
+				canvas.style["width"] = canvas.width
+				canvas.style["height"] = canvas.height
+			}
 		}
 		
-		const resize = (stage) => {
-			const { canvas } = stage.context
-			canvas.width = Math.round(innerWidth)
-			canvas.height = Math.round(innerHeight)
-			canvas.style["width"] = canvas.width
-			canvas.style["height"] = canvas.height
-			stage.resize(stage.context)
-		}
-		
-		const tick = (stage, time) => {
-			stage.clock += stage.speed
-			while (stage.clock > 0) {
-				if (!stage.paused) stage.update(stage.context, time)
-				stage.tick(stage.context, time)
-				stage.clock--
+		const Layer = class {
+			static types = {
+				"2d": CanvasLayer,
 			}
 		
-			requestAnimationFrame((time) => tick(stage, time))
+			constructor(type) {
+				const Constructor = Layer.types[type]
+				return new Constructor()
+			}
+		
+			start() {}
+			resize() {}
 		}
 		
 
