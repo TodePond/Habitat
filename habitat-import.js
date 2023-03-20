3a5518ebb3dfc0409d357a4957d1574e295a3854
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-03-20 22:48:04 +0000

    optios

--- habitat-import.js
+++ habitat-import.js
@@ -227,19 +227,30 @@ const HabitatFrogasaurus = {}
 		const Options = class extends Function {
 			constructor(head, tail = {}) {
 				super("head", "tail = {}", "return this.parse(head, tail)")
+				const options = parse("default", {}, [head, tail])
+				Object.assign(this, options)
 			}
 		
 			parse(head, tail = {}) {
-				return parse(this.default, this.defaults, [head, tail])
+				return parse(this.default, this.defaultValues, [head, tail])
 			}
 		}
 		
-		const parse = (_default, defaults = {}, [head, tail = {}]) => {
-			return {
-				...defaults,
+		const parse = (defaultOption, getDefaultValues = {}, [head, tail = {}]) => {
+			const isDefaultOption = typeof head !== "object"
+			const source = isDefaultOption ? tail : head
+			const options = {}
 		
-				...tail,
+			if (typeof head !== "object") {
+				options[defaultOption] = head
 			}
+		
+			for (const key in getDefaultValues) {
+				const value = source[key]
+				options[key] = value !== undefined ? value : getDefaultValues[key]()
+			}
+		
+			return options
 		}
 		
 
