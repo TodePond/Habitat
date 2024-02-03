593823c044f42ffbf260c71c69b217ea97f3f382
Lu[ke] Wilson <l2wilson94@gmail.com>
2024-02-03 19:19:03 +0000

    signal

--- habitat.js
+++ habitat.js
@@ -1,67 +1,46 @@
-//=======//
-// ARRAY //
-//=======//
-/**
- * Shuffle an array in place.
- * @template T
- * @param {T[]} array - The array to shuffle.
- * @returns {T[]}
- */
-export function shuffle(array) {
-  // Go backwards through the array
-  // Swap each value with a random value before it (which might include itself)
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-//=======//
-// ASYNC //
-//=======//
-/**
- * Wait for a given duration.
- * @param {number} duration
- */
-export async function sleep(duration) {
-  new Promise((resolve) => setTimeout(resolve, duration));
-}
-
 //========//
 // COLOUR //
 //========//
 /**
  * Convert a colour to a hex string.
- * @param {Colour} colour - The colour to convert.
- * @returns {string}
+ * @param {[number, number, number, number?]} colour
  */
-export function toHex(colour) {
-  return colour
-    .filter((v) => v !== undefined)
+export function toHex([red, green, blue, alpha = 255]) {
+  return [red, green, blue, alpha]
     .map((v) => v?.toString(16).padStart(2, "0"))
     .join("");
 }
 
-export const VOID = c([6, 7, 10]);
-export const BLACK = c([23, 29, 40]);
-export const DARK_GREY = c([31, 39, 54]);
-export const GREY = c([55, 67, 98]);
-export const LIGHT_GREY = c([87, 101, 147]);
-export const SILVER = c([159, 174, 204]);
-export const WHITE = c([255, 255, 255]);
-export const GREEN = c([70, 255, 128]);
-export const CYAN = c([70, 204, 255]);
-export const BLUE = c([70, 128, 255]);
-export const PURPLE = c([128, 67, 255]);
-export const PINK = c([255, 128, 222]);
-export const CORAL = c([255, 128, 128]);
-export const RED = c([255, 67, 70]);
-export const ORANGE = c([255, 128, 70]);
-export const YELLOW = c([255, 255, 70]);
+export const GREEN = [70, 255, 128];
+export const CYAN = [70, 204, 255];
+export const BLUE = [70, 128, 255];
+export const PURPLE = [128, 67, 255];
+export const PINK = [255, 128, 222];
+export const CORAL = [255, 128, 128];
+export const RED = [255, 67, 70];
+export const ORANGE = [255, 128, 70];
+export const YELLOW = [255, 255, 70];
 
 /** All monochrome colours. */
-export const SHADES = [VOID, BLACK, DARK_GREY, GREY, LIGHT_GREY, SILVER, WHITE];
+export const SHADES = [
+  [6, 7, 10], //VOID
+  [14, 16, 22],
+  [24, 29, 39], //BLACK
+  [33, 39, 53], //DARK_GREY
+  [47, 56, 75],
+  [55, 67, 98], //GREY
+  [87, 101, 147], //LIGHT_GREY
+  [135, 146, 171],
+  [159, 174, 204], //SILVER
+  [224, 224, 224],
+  [255, 255, 255], //WHITE
+];
+
+export const VOID = SHADES[0];
+export const BLACK = SHADES[2];
+export const GREY = SHADES[5];
+export const SILVER = SHADES[8];
+export const WHITE = SHADES[10];
 
 /** All colourful colours. (?) */
 export const HUES = [
@@ -79,194 +58,65 @@ export const HUES = [
 /** All colours. */
 export const COLOURS = [...SHADES, ...HUES];
 
-//===========//
-// COMPONENT //
-//===========//
-
-//=========//
-// CONSOLE //
-//=========//
-/** Print to the console. */
-export const print = console.log.bind(console);
-
-/** Register the .d debugger */
-export function registerDebugger() {
-  Reflect.defineProperty(Object.prototype, "d", {
-    get() {
-      const value = this.valueOf();
-      console.log(value);
-      return value;
-    },
-    set(value) {
-      Reflect.defineProperty(this, "d", {
-        value,
-        configurable: true,
-        writable: true,
-        enumerable: true,
-      });
-    },
-    configurable: true,
-    enumerable: false,
-  });
-}
-
-//==========//
-// DOCUMENT //
-//==========//
+//========//
+// SIGNAL //
+//========//
 /**
- * Select an element from the document.
- * @param {string} selector
+ * @template T
  */
-export function $(selector) {
-  return document.querySelector(selector);
-}
+export class Signal {
+  dependants = new Set();
 
-/**
- * Select all elements from the document.
- * @param {string} selector
- */
-export function $$(selector) {
-  return document.querySelectorAll(selector);
-}
-
-//======//
-// HTML //
-//======//
-/**
- * Generate an element from some HTML.
- * @param {string} tag
- * @param {Object.<string, string>} attributes
- * @param {Element[]} children
- * @returns {Element}
- */
-export function HTML(tag, attributes, children) {
-  const element = document.createElement(tag);
-  for (const [key, value] of Object.entries(attributes)) {
-    element.setAttribute(key, value);
-  }
-  for (const child of children) {
-    element.appendChild(child);
-  }
-  return element;
-}
-
-//============//
-// JAVASCRIPT //
-//============//
-/**
- * Evaluate some JavaScript.
- * @param {string} source
- */
-export function JS(source) {
-  const func = new Function("return " + source);
-  return func();
-}
-
-//======//
-// LERP //
-//======//
-// export function lerp([start, end], distance) {
-//   const range = subtract(start, end);
-//   const displacement = scale(range, distance);
-//   return add(start, displacement);
-// }
-
-//=============//
-// LINKED LIST //
-//=============//
-export const LinkedList = class {
   /**
-   * @param {Iterable} iterable
+   * Create a new signal.
+   * @param {(previous?: T) => T|T} template
+   * @param {Signal[]} dependencies
+   * @param {(a: T, b: T) => boolean} equals
    */
-  constructor(iterable = []) {
-    this.start = undefined;
-    this.end = undefined;
-    this.isEmpty = true;
+  constructor(template, dependencies = [], equals = (a, b) => a === b) {
+    this.equals = equals;
+    this.dependencies = dependencies;
 
-    for (const value of iterable) {
-      this.push(value);
+    for (const dependency of dependencies) {
+      dependency.dependants.add(this);
+    }
+
+    if (typeof template === "function") {
+      this.callback = template;
+      this.value = this.callback();
+    } else {
+      this.value = template;
     }
   }
 
-  *[Symbol.iterator]() {
-    let link = this.start;
-    while (link !== undefined) {
-      yield link.value;
-      link = link.next;
+  update() {
+    if (this.callback) {
+      this.value = this.callback(this.value);
+    }
+    for (const dependant of this.dependants) {
+      dependant.update();
     }
   }
 
-  toString() {
-    return [...this].toString();
+  /**
+   * @returns {T}
+   */
+  get() {
+    return this.value;
   }
 
-  push(...values) {
-    for (const value of values) {
-      const link = makeLink(value);
-      if (this.isEmpty) {
-        this.start = link;
-        this.end = link;
-        this.isEmpty = false;
-      } else {
-        this.end.next = link;
-        link.previous = this.end;
-        this.end = link;
-      }
-    }
+  /**
+   * @param {T} value
+   */
+  set(value) {
+    if (this.equals(this.value, value)) return;
+    this.value = value;
+    this.update();
   }
 
-  pop() {
-    if (this.isEmpty) {
-      return undefined;
+  dispose() {
+    for (const dependency of this.dependencies) {
+      dependency.dependants.delete(this);
     }
-
-    const value = this.start.value;
-    if (this.start === this.end) {
-      this.clear();
-      return value;
-    }
-
-    this.end = this.end.previous;
-    this.end.next = undefined;
-    return value;
   }
-
-  shift() {
-    if (this.isEmpty) {
-      return undefined;
-    }
-
-    const value = this.start.value;
-    if (this.start === this.end) {
-      this.clear();
-      return value;
-    }
-
-    this.start = this.start.next;
-    this.start.previous = undefined;
-    return value;
-  }
-
-  clear() {
-    this.start = undefined;
-    this.end = undefined;
-    this.isEmpty = true;
-  }
-};
-
-//======//
-// TYPE //
-//======//
-/** @type {AsConstant} */
-export function c(v) {
-  return v;
 }
-
-/** @type {AsTuple} */
-export function t(v) {
-  return v;
-}
-
-//========//
-// VECTOR //
-//========//
